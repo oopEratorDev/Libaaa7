fe0417d0634c2cc680f33dbfe491b30bbb2a96a7
oopEratorDev <ooperator2001@gmail.com>
2023-08-12 17:01:57 +0700

    Initial commit

---
+++ libraryandroid7/src/main/java/com/ooperator/libraryandroid7/LibAndroid7.java
@@ -0,0 +1,10 @@
+package com.ooperator.libraryandroid7;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class LibAndroid7 {
+    public void Toast(Context context) {
+        Toast.makeText(context, "Hello Libandroid7", Toast.LENGTH_SHORT).show();
+    }
+}
